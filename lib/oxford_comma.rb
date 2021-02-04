83116bd295a0c52089a8e6953753df1cbc9f5428
Rene <renegarcia1864@gmail.com>
2021-02-03 18:07:30 -0600

    commit tree 661c3dd430d1d0e31c8a25c62026d02c87aec809

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
+require 'pry'
+
 def oxford_comma(array)
     if array.length == 1
-return "#{array[0]}"
+        "#{array[0]}"
     elsif array.length == 2
-        return array.join(" and ")
+        "#{array.join(' and ')}"
     elsif array.length >= 3
-       array[-1] = "and #{array[-1]}"
-       return array.join(", ")
-
+        array[-1] = "and #{array[-1]}"
+        "#{array.join(', ')}"
     end
 end
-
-
-
